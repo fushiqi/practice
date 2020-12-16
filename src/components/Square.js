a8bf0d17d3b6f9f9f52c6dfd5734b5ab003b9879
Wenwen Liao <wenwenliao@Wenwens-MacBook-Pro.local>
2020-12-15 22:44:09 -0800

    fix

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+import PropTypes from 'prop-types';
+
+Square.propTypes = {
+  value: PropTypes.string,
+  onClick: PropTypes.func
+};
 
 export default function Square(props) {
     return (
